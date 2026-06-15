10eccc19ffe080cc5056ebedee43346fef0f39c1
wer2303 <wer2303.v2@gamil.com>
2026-06-15 19:34:29 +0300

    sss

--- src/client/java/net/sadvistan/sadvistan/client/SadvistanClient.java
+++ src/client/java/net/sadvistan/sadvistan/client/SadvistanClient.java
@@ -1,10 +1,41 @@
 package net.sadvistan.sadvistan.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
+import net.sadvistan.sadvistan.Sadvistan;
+import org.lwjgl.glfw.GLFW;
 
 public class SadvistanClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    private static KeyMapping playMusicKey;
+    private static SimpleSoundInstance currentSoundInstance = null;
+
+    @Override
+    public void onInitializeClient() {
+        playMusicKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+                "key.sadvistan.play",
+                GLFW.GLFW_KEY_H,
+                "category.sadvistan.mod"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (playMusicKey.consumeClick()) {
+                if (client.player != null && client.level != null) {
+
+                    if (currentSoundInstance != null && client.getSoundManager().isActive(currentSoundInstance)) {
+
+                        client.getSoundManager().stop(currentSoundInstance);
+                        currentSoundInstance = null;
+
+                    } else {
+
+                        currentSoundInstance = SimpleSoundInstance.forUI(Sadvistan.MY_MUSIC_EVENT, 1.0F, 1.0F);
+                        client.getSoundManager().play(currentSoundInstance);
+                    }
+                }
+            }
+        });
+    }
 }
